840e745dfd1f1b615aba0a71ee5a148f18ec5c8b
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-02-10 09:41:12 +0300

    prettier, eslint setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={`${geistSans.variable} font-poppins antialiased`}>{children}</body>
     </html>
   );
