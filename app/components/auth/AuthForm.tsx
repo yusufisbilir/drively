94f91a6a25825dd531857d44a4933afaaaa7b9da
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-02-10 21:38:49 +0300

    add auth zod validation

--- app/components/auth/AuthForm.tsx
+++ app/components/auth/AuthForm.tsx
@@ -1,5 +1,16 @@
+import { z } from 'zod';
+
+type AuthFormType = 'sign-in' | 'sign-up';
+
 type Props = {
-  type: 'sign-in' | 'sign-up';
+  type: AuthFormType;
+};
+
+const authFormSchema = (formType: AuthFormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName: formType === 'sign-up' ? z.string().min(2).max(50) : z.string().optional(),
+  });
 };
 
 const AuthForm = ({ type }: Props) => {
