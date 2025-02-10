317f7ccae78d7f347338620afe6158d26038a12f
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-02-11 00:02:17 +0300

    add form inputs

--- app/components/auth/AuthForm.tsx
+++ app/components/auth/AuthForm.tsx
@@ -1,4 +1,19 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 
 type AuthFormType = 'sign-in' | 'sign-up';
 
@@ -6,15 +21,64 @@ type Props = {
   type: AuthFormType;
 };
 
-const authFormSchema = (formType: AuthFormType) => {
-  return z.object({
-    email: z.string().email(),
-    fullName: formType === 'sign-up' ? z.string().min(2).max(50) : z.string().optional(),
-  });
-};
-
 const AuthForm = ({ type }: Props) => {
-  return <div>authForm</div>;
+  const authFormSchema = (formType: AuthFormType) => {
+    return z.object({
+      email: z.string().email(),
+      fullName: formType === 'sign-up' ? z.string().min(2).max(50) : z.string().optional(),
+    });
+  };
+
+  const formSchema = authFormSchema(type);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: '',
+      email: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="example@example.com" {...field} />
+              </FormControl>
+              <FormDescription>We'll never share your email with anyone else.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {type === 'sign-up' && (
+          <FormField
+            control={form.control}
+            name="fullName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Full Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} />
+                </FormControl>
+                <FormDescription>Please enter your full name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        )}
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default AuthForm;
