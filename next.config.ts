68cddce71d9e4d6e14a761c6b8e54aaf50e735cb
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-02-18 21:28:06 +0300

    add appwrite url to config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'img.freepik.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+      },
     ],
   },
 };
