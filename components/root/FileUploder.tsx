f972f0796cbfed6c0a812fd9368a9d0859a6b5b3
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-02-20 16:41:32 +0300

    file uploader

--- components/root/FileUploder.tsx
+++ components/root/FileUploder.tsx
@@ -2,14 +2,43 @@
 
 import { Upload } from 'lucide-react';
 import { Button } from '../ui/button';
+import { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 
-const FileUploader = () => {
+const FileUploader = ({
+  ownerId,
+  userId,
+  className,
+}: {
+  ownerId: string;
+  userId: string;
+  className?: string;
+}) => {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const handleRemoveFile = (
+    e: React.MouseEvent<HTMLImageElement, MouseEvent>,
+    fileName: string
+  ) => {
+    e.stopPropagation();
+    setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName));
+  };
+
   return (
-    <div className="cursor-pointer">
-      <Button type="button" className="bg-blue-600 text-white hover:bg-blue-700 px-4">
-        <Upload className="h-5 w-5" />
-        <span className="ml-2 font-medium">Upload</span>
-      </Button>
+    <div className="cursor-pointer" {...getRootProps()}>
+      <div>
+        <input {...getInputProps()} />
+        <Button type="button" className="bg-blue-600 text-white hover:bg-blue-700 px-4">
+          <Upload className="h-5 w-5" />
+          Upload
+        </Button>
+      </div>
     </div>
   );
 };
