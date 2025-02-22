e1484a50b1404790df5d670d2837f81a011a5225
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-02-22 11:49:21 +0300

    add uploadFile action

--- lib/actions/file.acions.ts
+++ lib/actions/file.acions.ts
@@ -1,3 +1,53 @@
 'use server';
 
-export const uploadFile = async ({ file, ownerId, userId, path }: UploadFileProps) => {};
+import { createAdminClient } from '../appwrite';
+import { InputFile } from 'node-appwrite/file';
+import { appwriteConfig } from '../appwrite/config';
+import { ID } from 'node-appwrite';
+import getFileType from '@/utils/getFileType';
+import { constructFileUrl } from '@/utils/appwriteUtils';
+import { revalidatePath } from 'next/cache';
+import parseStringify from '@/utils/parseStringify';
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+export const uploadFile = async ({ file, ownerId, userId, path }: UploadFileProps) => {
+  const { storage, databases } = await createAdminClient();
+
+  try {
+    const inputFile = InputFile.fromBuffer(file, file.name);
+    const bucketFile = await storage.createFile(appwriteConfig.bucketId, ID.unique(), inputFile);
+
+    const fileDocument = {
+      type: getFileType(bucketFile.name).type,
+      name: bucketFile.name,
+      url: constructFileUrl(bucketFile.$id),
+      extension: getFileType(bucketFile.name).extension,
+      size: bucketFile.sizeOriginal,
+      owner: ownerId,
+      accountId: userId,
+      users: [],
+      bucketFileId: bucketFile.$id,
+    };
+
+    const newFile = await databases
+      .createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.filesCollectionId,
+        ID.unique(),
+        fileDocument
+      )
+      .catch((error) => {
+        storage.deleteFile(appwriteConfig.bucketId, bucketFile.$id);
+        handleError(error, 'Failed to create file document');
+      });
+
+    revalidatePath(path);
+    return parseStringify(newFile);
+  } catch (error) {
+    handleError(error, 'Failed to upload file');
+  }
+};
---
+++ utils/appwriteUtils.ts
@@ -0,0 +1,3 @@
+export const constructFileUrl = (bucketFileId: string) => {
+  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/view?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`;
+};
