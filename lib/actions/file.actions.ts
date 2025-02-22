24b90db36f40eb51aeea12bcd30af0ecb9d2da20
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-02-22 22:51:55 +0300

    remove log

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -100,7 +100,6 @@ export const getFiles = async ({
       appwriteConfig.filesCollectionId,
       queries
     );
-    console.log({ files });
 
     return parseStringify(files);
   } catch (error) {
