6f86a89fbff017758acbe38e78d682c29f3d2772
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-02-22 22:19:21 +0300

    add getFiles action

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -3,11 +3,12 @@
 import { createAdminClient } from '../appwrite';
 import { InputFile } from 'node-appwrite/file';
 import { appwriteConfig } from '../appwrite/config';
-import { ID } from 'node-appwrite';
+import { ID, Models, Query } from 'node-appwrite';
 import getFileType from '@/utils/getFileType';
 import { constructFileUrl } from '@/utils/appwriteUtils';
 import { revalidatePath } from 'next/cache';
 import parseStringify from '@/utils/parseStringify';
+import { getCurrentUser } from './user.actions';
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -49,5 +50,60 @@ export const uploadFile = async ({ file, ownerId, userId, path }: UploadFileProp
     return parseStringify(newFile);
   } catch (error) {
     handleError(error, 'Failed to upload file');
+  }
+};
+
+const createQueries = (
+  currentUser: Models.Document,
+  types: string[],
+  searchText: string,
+  sort: string,
+  limit?: number
+) => {
+  const queries = [
+    Query.or([
+      Query.equal('owner', [currentUser.$id]),
+      Query.contains('users', [currentUser.email]),
+    ]),
+  ];
+
+  if (types.length > 0) queries.push(Query.equal('type', types));
+  if (searchText) queries.push(Query.contains('name', searchText));
+  if (limit) queries.push(Query.limit(limit));
+
+  if (sort) {
+    const [sortBy, orderBy] = sort.split('-');
+
+    queries.push(orderBy === 'asc' ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy));
+  }
+
+  return queries;
+};
+
+export const getFiles = async ({
+  types = [],
+  searchText = '',
+  sort = '$createdAt-desc',
+  limit,
+}: GetFilesProps) => {
+  const { databases } = await createAdminClient();
+
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) throw new Error('User not found');
+
+    const queries = createQueries(currentUser, types, searchText, sort, limit);
+
+    const files = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      queries
+    );
+
+    console.log({ files });
+    return parseStringify(files);
+  } catch (error) {
+    handleError(error, 'Failed to get files');
   }
 };
