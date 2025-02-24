d8ea4390818231c45926b6fe953d495adee10509
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-02-24 21:47:06 +0300

    fix env keys

--- utils/appwriteUtils.ts
+++ utils/appwriteUtils.ts
@@ -1,3 +1,3 @@
 export const constructFileUrl = (bucketFileId: string) => {
-  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/view?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`;
+  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID}/files/${bucketFileId}/view?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`;
 };
