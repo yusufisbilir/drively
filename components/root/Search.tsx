da3f07461886f67cc95f0923e1b2eaf5b9308efc
yusufisbilir <dev.yusufisbilir@gmail.com>
2025-03-01 21:25:00 +0300

    search functionality

--- components/root/Search.tsx
+++ components/root/Search.tsx
@@ -1,12 +1,57 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Search as SearchIcon } from 'lucide-react';
 import { Input } from '@/components/ui/input';
+import { useDebounce } from 'use-debounce';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { Models } from 'node-appwrite';
+import { getFiles } from '@/lib/actions/file.actions';
+import Thumbnail from './Thumbnail';
+import FormattedDateTime from './type/FormattedDateTime';
 
 const Search = () => {
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get('query') || '';
+  const router = useRouter();
+  const path = usePathname();
+
   const [query, setQuery] = useState('');
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+  const [debouncedQuery] = useDebounce(query, 300);
   const [isFocused, setIsFocused] = useState(false);
+
+  const handleClickItem = (file: Models.Document) => {
+    setOpen(false);
+    setResults([]);
+
+    router.push(
+      `/${file.type === 'video' || file.type === 'audio' ? 'media' : file.type + 's'}?query=${query}`
+    );
+  };
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      if (debouncedQuery.length === 0) {
+        setResults([]);
+        setOpen(false);
+        return router.push(path.replace(searchParams.toString(), ''));
+      }
+
+      const files = await getFiles({ types: [], searchText: debouncedQuery });
+      setResults(files.documents);
+      setOpen(true);
+    };
+
+    fetchFiles();
+  }, [debouncedQuery]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery('');
+    }
+  }, [searchQuery]);
 
   return (
     <div className="relative w-full md:max-w-[480px]">
@@ -23,6 +68,36 @@ const Search = () => {
           onBlur={() => setIsFocused(false)}
         />
       </div>
+      {open && (
+        <ul className="absolute left-0 top-16 z-50 flex w-full flex-col gap-3 rounded-[20px] bg-white p-4">
+          {results.length > 0 ? (
+            results.map((file) => (
+              <li
+                className="flex items-center justify-between"
+                key={file.$id}
+                onClick={() => handleClickItem(file)}
+              >
+                <div className="flex cursor-pointer items-center gap-4">
+                  <Thumbnail
+                    type={file.type}
+                    extension={file.extension}
+                    url={file.url}
+                    className="size-9 min-w-9"
+                  />
+                  <p className="subtitle-2 line-clamp-1 text-light-100">{file.name}</p>
+                </div>
+
+                <FormattedDateTime
+                  date={file.$createdAt}
+                  className="caption line-clamp-1 text-light-200"
+                />
+              </li>
+            ))
+          ) : (
+            <p>No files found</p>
+          )}
+        </ul>
+      )}
     </div>
   );
 };
